6e7c0c77595ded50618c8dd22f2c403248b986b5
Tom Ford-Knowles <thomas.ford-knowles@hotmail.co.uk>
2019-04-30 09:11:26 +0100

    Added PHP Validation
    First step of section 2 is done with basic validation done so this will be commit and the code will be refactored

--- my-form-handling-page.php
+++ my-form-handling-page.php
@@ -10,22 +10,6 @@
     <?php
         echo "<p>Welcome to PHP</p>";
 
-
-
-
-        $question4 = $_POST['city'];
-        $question5 = $_POST['pole'];
-        $question6 = $_POST['question6'];
-        $question7 = $_POST['question7'];
-        $question8 = $_POST['question8'];
-        $question9 = $_POST['question9'];
-        $question10Slider = $_POST['sliderOne'];
-        $question10NumberInput = $_POST['numberInputOne'];
-        $question10Result = $_POST['sliderOne'] + $_POST['numberInputOne'];
-        $question11Slider = $_POST['sliderTwo'];
-        $question11NumberInput = $_POST['numberInputTwo'];
-        $question11Result = $_POST['sliderTwo'] + $_POST['numberInputTwo'];
-
         //Declaring a score keeping variable
 
         $quizScore = 0;
@@ -114,20 +98,127 @@
             echo "<div> <b>Question 3 Answer:</b> <br/> Not answered <hr></div>";
         };
 
-        //Validation on Q4
-
-        /*
-        foreach($_POST['city'] as $selected){
-            echo "<div>$selected</div>";
+        //Validation on Q4 - multi choice check box, one answer
+        if ( isset($_POST['city']) ){
+            $question4 = $_POST['city'];
+            $answers = ['Rome'];
+            $question4Answers = array_intersect($question4, $answers);
+            if ($question4Answers === $answer){
+                echo "<div> <b>Question 4 Answers:</b> <br/>" . join('<br/>', $question4) . "</div>";
+                echo "<div> <b>Result:</b> <br/> Correct <hr></div>";
+                $quizScore += 1;
+            } else {
+                echo "<div> <b>Question 4 Answers:</b> <br/>" . join('<br/>', $question4) . "</div>";
+                echo "<div> <b>Result:</b> <br/> Incorrect <hr></div>";
+                $quizScore -= 1;
+            }
+        } else {
+            echo "<div> <b>Question 4 Answer:</b> <br/> Not answered <hr></div>";
         };
 
-        foreach($_POST['pole'] as $selected) {
-            echo "<div>$selected</div>";
+        //Validation on Q5 - multi choice check box, two answers
+        if ( isset($_POST['pole']) ){
+            $question5 = $_POST['pole'];
+            $answers = ['Arctic', 'Antarctic'];
+            $question5Answers = array_intersect($question5, $answers);
+            if ($question5Answers === $answers){
+                echo "<div> <b>Question 5 Answers:</b> <br/>" . join('<br/>', $question5) . "</div>";
+                echo "<div> <b>Result:</b> <br/> Correct <hr></div>";
+                $quizScore += 1;
+            } else {
+                echo "<div> <b>Question 5 Answers:</b> <br/>" . join('<br/>', $question5) . "</div>";
+                echo "<div> <b>Result:</b> <br/> Incorrect <hr></div>";
+                $quizScore -= 1;
+            }
+        } else {
+            echo "<div> <b>Question 5 Answer:</b> <br/> Not answered <hr></div>";
         };
-        */
 
+        //Validation on Q6 - must contain specific string
+        if ( isset($_POST['question6']) ){
+            $question6 = $_POST['question6'];
+            if (strpos($question6, 'Whitehouse') !== false){
+                echo "<div> <b>Question 6 Answer:</b> <br/> $question6 </div>";
+                echo "<div> <b>Result:</b> <br/> Correct <hr></div>";
+                $quizScore += 1;
+            } else {
+                echo "<div> <b>Question 6 Answer:</b> <br/> $question6 </div>";
+                echo "<div> <b>Result:</b> <br/> Incorrect <hr></div>";
+                $quizScore -= 1;
+            }
+        } else {
+            echo "<div> <b>Question 6 Answer:</b> <br/> Not answered <hr></div>";
+        };
 
+        //Validation on Q7 - must be specific string
+        if ( isset($_POST['question7']) ){
+            $question7 = $_POST['question7'];
+            if ($question7 === "George Washington"){
+                echo "<div> <b>Question 7 Answer:</b> <br/> $question7 </div>";
+                echo "<div> <b>Result:</b> <br/> Correct <hr></div>";
+                $quizScore += 1;
+            } else {
+                echo "<div> <b>Question 7 Answer:</b> <br/> $question7 </div>";
+                echo "<div> <b>Result:</b> <br/> Incorrect <hr></div>";
+                $quizScore -= 1;
+            }
+        } else {
+            echo "<div> <b>Question 7 Answer:</b> <br/> Not answered <hr></div>";
+        };
 
+        //Validation on Q8 - must be more than a certain number
+        if ( isset($_POST['question8']) ){
+            $question8 = $_POST['question8'];
+            if($question8 > 400){
+                echo "<div> <b>Question 8 Answer:</b> <br/> $question8 </div>";
+                echo "<div> <b>Result:</b> <br/> Correct <hr></div>";
+                $quizScore += 1;
+            } else {
+                echo "<div> <b>Question 8 Answer:</b> <br/> $question8 </div>";
+                echo "<div> <b>Result:</b> <br/> Incorrect <hr></div>";
+                $quizScore -= 1;
+            }
+        } else {
+            echo "<div> <b>Question 8 Answer:</b> <br/> Not answered <hr></div>";
+        };
+
+        //Validation on Q9 - just needs to check whether empty or not
+        if ( isset($_POST['question9']) ){
+            $question9 = $_POST['question9'];
+            echo "<div> <b>Question 9 Answer:</b> <br/> $question9 <hr></div>";
+        } else {
+            echo "<div> <b>Question 9 Answer:</b> <br/> Not answered <hr></div>";
+        };
+
+        //Validation on Q10 - needs to be exact number
+        //These will always be set and have default values
+        $question10Slider = $_POST['sliderOne'];
+        $question10NumberInput = $_POST['numberInputOne'];
+        $question10Result = $_POST['sliderOne'] + $_POST['numberInputOne'];
+        if ($question10Result === 150){
+            echo "<div> <b>Question 10 Answer:</b> <br/> $question10Result </div>";
+            echo "<div> <b>Result:</b> <br/> Correct <hr></div>";
+            $quizScore += 1;
+        } else {
+            echo "<div> <b>Question 10 Answer:</b> <br/> $question10Result </div>";
+            echo "<div> <b>Result:</b> <br/> Incorrect <hr></div>";
+            $quizScore -= 1;
+        };
+
+        //Validation on Q11 - needs to be exact number
+        //These will always be set and have default values
+        $question11Slider = $_POST['sliderTwo'];
+        $question11NumberInput = $_POST['numberInputTwo'];
+        $question11Result = $_POST['sliderTwo'] + $_POST['numberInputTwo'];
+        if ($question11Result === 600){
+            echo "<div> <b>Question 11 Answer:</b> <br/> $question11Result </div>";
+            echo "<div> <b>Result:</b> <br/> Correct <hr></div>";
+            $quizScore += 1;
+        } else {
+            echo "<div> <b>Question 11 Answer:</b> <br/> $question11Result </div>";
+            echo "<div> <b>Result:</b> <br/> Incorrect <hr></div>";
+            $quizScore -= 1;
+        };
         echo "<pre>";
         print_r($_POST);
         print_r($_SERVER);
