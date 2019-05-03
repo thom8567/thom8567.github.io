8dbd1571e9316b7b29a04dc0b9912154bf2b1a45
Tom Ford-Knowles <thomas.ford-knowles@hotmail.co.uk>
2019-05-03 15:57:46 +0100

    Improved Validation Further (WIP)

--- my-form-handling-page.php
+++ my-form-handling-page.php
@@ -17,8 +17,6 @@
         //Making sure that POST is correct and is an array
         $answers = ( array ) ( $_POST ?? [] );
 
-        print_r($answers);
-
         $rules = [
             'fullName' => [
                 'required' => true,
@@ -33,11 +31,11 @@
                 'phoneNumber' => true,
             ],
             'question1' => [
-                'correctAnswer' => 'Stars',
+                'correctChoices' => ['Stars'],
                 'answerNeeded'  => false,
             ],
             'question2' => [
-                'correctAnswer' => 'Tiger',
+                'correctChoices' => ['Tiger'],
                 'answerNeeded'  => true,
             ],
             'question3' => [
@@ -53,27 +51,27 @@
                 'answerNeeded'   => true,
             ],
             'question6' => [
-                'correctAnswer' => 'Whitehouse',
-                'answerNeeded'  => true,
+                'correctChoices' => ['Whitehouse'],
+                'answerNeeded'   => true,
             ],
             'question7' => [
-                'correctAnswer' => 'George Washington',
-                'answerNeeded'  => true,
+                'correctChoices' => ['George Washington'],
+                'answerNeeded'   => true,
             ],
             'question8' => [
-                'correctAnswer' => 400,
-                'answerNeeded'  => true,
+                'correctChoices' => [400],
+                'answerNeeded'   => true,
             ],
             'question9' => [
                 'answerNeeded' => true
             ],
             'question10' => [
-                'correctAnswer' => 150,
-                'answerNeeded'  => true,
+                'correctChoices' => [150],
+                'sliderInput'   => true,
             ],
             'question11' => [
-                'correctAnswer' => 600,
-                'answerNeeded'  => true,
+                'correctChoices' => [600],
+                'sliderInput'   => true,
             ]
         ];
 
@@ -81,252 +79,135 @@
 
         function validate($answers, $key, array $ruleSet){
             try {
-                if ( isset($ruleSet['required']) && $ruleSet['required'] ){
-                    assertFilledOut( $answers, $key );
+                if( isset($ruleSet['required']) && $ruleSet['required'] ){
+                    assertFilledOut( $answers, $key, $ruleSet );
                 }
-                if ( isset($ruleSet['answerNeeded']) && $ruleSet['answerNeeded'] ){
+                if( isset($ruleSet['answerNeeded']) && $ruleSet['answerNeeded'] ){
                     assertAnswered( $answers, $key );
                 }
-                if ( isset($ruleSet['correctChoices']) && is_array($ruleSet['correctChoices']) ){
-                    assertCheckAnswer($answers, $key, $ruleSet['correctChoices']);
+                if( isset($ruleSet['sliderInput']) && $ruleSet['sliderInput'] ){
+                    assertCheckCalculation( $answers, $key, $ruleSet['correctChoices'] );
                 }
-
+                if( isset($ruleSet['correctChoices']) && is_array($ruleSet['correctChoices']) ){
+                    assertCheckAnswer($answers, $key, $ruleSet['correctChoices'] );
+                }
             } catch ( \Exception $exception ) {
                 return $exception -> getMessage();
             }
         }
 
         //Check if question is answered
-        function assertFilledOut( array $answers, $key ){
+        function assertFilledOut( array $answers, $key, $ruleSet ){
             //Get question from answers array and return true/false depending on whether set or not
             if ( !isset($answers[$key]) ){
-                throw new \Exception('Required field has not been filled in');
-            }
-        }
-
-        function assertAnswered( array $answers, $key ){
-            if( !isset($answers[$key]) ){
-                throw new \Exception('Question has not been answered');
-            }
-        }
-
-        function assertCheckAnswer( array $answers, $key, array $choices ){
-            //Get user's answer from array
-            if ( !in_array($answers[$key], $choices) ){
-                throw new \Exception( 'Incorrect' );
-            }
-
-        }
-
-        echo "<div> $quizScore </div>";
-
-
-        //Validation on the user's full name
-        if ( isset($_POST['fullName']) ){
-            $fullName = $_POST['fullName'];
-            if ( !preg_match("/^[a-z ,.\'-]+$/i", $fullName)){
-                echo "<div> Invalid name given </div>";
+                throw new \Exception('Required field has not been filled in' );
             } else {
-                echo "<div> <b>Name:</b> $fullName </div>";
+                if ( strpos($key, 'email') ){
+                    if ( filter_var($answers[$key], FILTER_VALIDATE_EMAIL) ){
+                        throw new \Exception( 'Email is valid' );
+                    } else {
+                        throw new \Exception( 'Email is not valid' );
+                    }
+                } else if ( strpos($key, 'Name') ){
+                    if ( preg_match("/^[a-z ,.\'-]+$/i", $answers[$key]) ){
+                        throw new \Exception( 'Name is valid' );
+                    } else {
+                        throw new \Exception( 'Name is not valid' );
+                    }
+                } else if ( strpos($key, 'phone') ){
+                    if ( phoneNumberValidation($answers[$key]) ){
+                        throw new \Exception( 'Valid number' );
+                    } else {
+                        throw new \Exception( 'Invalid number' );
+                    }
+                }
             }
-        } else {
-            echo "<div> No name has been entered </div>";
-        };
+        }
 
+        function assertAnswered ( array $answers, $key ){
+            if ( empty($answers[$key]) ){
+                throw new \Exception('Question has not been answered' );
+            }
+        }
 
-        //Validation on the user's email
-        if ( isset($_POST['email'])){
-            $emailAddress = $_POST['email'];
-            if ( !filter_var($emailAddress, FILTER_VALIDATE_EMAIL) ){
-                echo "<div> Email address not valid </div>";
+        function assertCheckAnswer ( array $answers, $key, array $choices ){
+            //Check answer and perform different validation based on type
+            if ( is_numeric($answers[$key]) ){
+                $answer = (int)($answers[$key]);
+                if ( $answer === $choices[0] ){
+                    throw new \Exception( 'Correct!' );
+                } else {
+                    throw new \Exception( 'Incorrect!' );
+                }
+            } else if ( is_array($answers[$key]) ){
+                if ( $answers[$key] === $choices ){
+                    throw new \Exception( 'Correct!' );
+                } else {
+                    throw new \Exception( 'Incorrect!' );
+                }
+            } else if ( is_string($answers[$key]) ){
+                if ( strpos( $answers[$key], $choices[0] ) !== false ){
+                    throw new \Exception( 'Correct!' );
+                } else {
+                    throw new \Exception( 'Incorrect!' );
+                }
             } else {
-                echo "<div> <b>Email address:</b> $emailAddress </div>";
+                throw new \Exception( 'Unknown type' );
             }
-        } else {
-            echo "<div> No email has been entered </div>";
-        };
+        }
 
-        //Validation on the user's phone number
-        if ( isset($_POST['phoneNumber']) ){
-            $mobileNumber = $_POST['phoneNumber'];
-            $justNumbers = preg_replace("/[^0-9]/", '', $mobileNumber);
+        function assertCheckCalculation ( array $answers, $key, $choices ){
+            if ( empty($answers[$key]) ){
+                throw new \Exception( 'Question has not been answered' );
+            } else {
+                $range = (int)($answers[$key]['range'] ?? 0);
+                $number = (int)($answers[$key]['number'] ?? 0);
+                $result = $range + $number;
+                if ( $result === $choices[0] ){
+                    throw new \Exception( 'Correct!' );
+                } else {
+                    throw new \Exception( 'Incorrect!' );
+                }
+            }
+        }
+
+        function phoneNumberValidation($phoneNumber){
+            //Ensure that the number is just a number with no extra characters
+            $justNumbers = preg_replace('/[^0-9]/', '', $phoneNumber);
+
+            //Check if it has a leading number and eliminate it
             if ( strlen($justNumbers) == 11 ){
-                $justNumbers = preg_replace("/^[01]/", '', $justNumbers);
+                $justNumbers = preg_replace("/^1/", '',$justNumbers);
             }
+
+            //If there are 10 digits left the most likely, it is valid
             if ( strlen($justNumbers) == 10 ){
-                echo "<div><b>Phone number:</b> $mobileNumber <hr></div>";
-            }
-        } else {
-            echo "<div> No phone number has been entered <hr></div>";
-        };
-
-        //Validation on Q1 - which will always have a value so isset is not needed in this case
-        $question1 = $_POST['question1'];
-        if ($question1 === 'Stars'){
-            echo "<div> <b>Question 1 Answer:</b> <br/> $question1 </div>";
-            echo "<div> <b>Result:</b> <br/> Correct <hr></div>";
-            $quizScore += 1;
-        } else {
-            echo "<div> <b>Question 1 Answer:</b> <br/> $question1 </div>";
-            echo "<div> <b>Result:</b> <br/> Incorrect <hr></div>";
-            $quizScore -= 1;
-        };
-
-        //Validation on Q2 - can be empty so isset is needed
-        if ( isset($_POST['question2']) ){
-            $question2 = $_POST['question2'];
-            if ($question2 === 'Tiger'){
-                echo "<div> <b>Question 2 Answer:</b> <br/> $question2 </div>";
-                echo "<div> <b>Result:</b> <br/> Correct <hr></div>";
-                $quizScore += 1;
+                return true;
             } else {
-                echo "<div> <b>Question 2 Answer:</b> <br/> $question2 </div>";
-                echo "<div> <b>Result:</b> <br/> Incorrect <hr></div>";
-                $quizScore -= 1;
+                return false;
             }
-        } else {
-            echo "<div> <b>Question 2 Answer:</b> <br/> Not answered <hr></div>";
         }
 
-        //Validation on Q3 - multi choice dropdown
-        if ( isset($_POST['question3']) ){
-            $question3 = $_POST['question3'];
-            $answers = ['Audi', 'Lamborghini'];
-            $question3Answers = array_intersect($question3, $answers);
-            if ($question3Answers === $answers){
-                echo "<div> <b>Question 3 Answers:</b> <br/>" . join('<br/>', $question3) . "</div>";
-                echo "<div> <b>Result:</b> <br/> Correct <hr></div>";
-                $quizScore += 1;
-            } else {
-                echo "<div> <b>Question 3 Answers:</b> <br/>" . join('<br/>', $question3) . "</div>";
-                echo "<div> <b>Result:</b> <br/> Incorrect <hr></div>";
-                $quizScore -= 1;
-            }
-        } else {
-            echo "<div> <b>Question 3 Answer:</b> <br/> Not answered <hr></div>";
-        };
+        foreach ( $rules as $key => $rule ) {
+            $errors[$key] = validate($answers, $key, $rule);
+        }
 
-        //Validation on Q4 - multi choice check box, one answer
-        if ( isset($_POST['question4']) ){
-            $question4 = $_POST['question4'];
-            $answers = ['Rome'];
-            $question4Answers = array_intersect($question4, $answers);
-            if ($question4Answers === $answer){
-                echo "<div> <b>Question 4 Answers:</b> <br/>" . join('<br/>', $question4) . "</div>";
-                echo "<div> <b>Result:</b> <br/> Correct <hr></div>";
-                $quizScore += 1;
-            } else {
-                echo "<div> <b>Question 4 Answers:</b> <br/>" . join('<br/>', $question4) . "</div>";
-                echo "<div> <b>Result:</b> <br/> Incorrect <hr></div>";
-                $quizScore -= 1;
-            }
-        } else {
-            echo "<div> <b>Question 4 Answer:</b> <br/> Not answered <hr></div>";
-        };
+        $questionKeys = array_keys($errors);
 
-        //Validation on Q5 - multi choice check box, two answers
-        if ( isset($_POST['question5']) ){
-            $question5 = $_POST['question5'];
-            $answers = ['Arctic', 'Antarctic'];
-            $question5Answers = array_intersect($question5, $answers);
-            if ($question5Answers === $answers){
-                echo "<div> <b>Question 5 Answers:</b> <br/>" . join('<br/>', $question5) . "</div>";
-                echo "<div> <b>Result:</b> <br/> Correct <hr></div>";
-                $quizScore += 1;
-            } else {
-                echo "<div> <b>Question 5 Answers:</b> <br/>" . join('<br/>', $question5) . "</div>";
-                echo "<div> <b>Result:</b> <br/> Incorrect <hr></div>";
-                $quizScore -= 1;
-            }
-        } else {
-            echo "<div> <b>Question 5 Answer:</b> <br/> Not answered <hr></div>";
-        };
+        foreach ( $questionKeys as $questionNumber ){
+            $response = $errors[$questionNumber];
+            $questionNumber = preg_replace('/(?<!\ )[A-Z]/', ' $0', $questionNumber);
+            //CURRENTLY NOT VALIDATING AGAINST DOUBLE DIGITS CORRECTLY
+            $questionNumber = preg_replace('/(?<!\ )[0-9]/', ' $0', $questionNumber);
+            $questionNumber = preg_replace('/(?<!\ )[0-9][0-9]/', ' $0', $questionNumber);
+            $questionNumber = ucwords($questionNumber);
+            echo "<div> <b>$questionNumber</b> <br/> $response <hr/> </div>";
+        }
 
-        //Validation on Q6 - must contain specific string
-        if ( isset($_POST['question6']) ){
-            $question6 = $_POST['question6'];
-            if (strpos($question6, 'Whitehouse') !== false){
-                echo "<div> <b>Question 6 Answer:</b> <br/> $question6 </div>";
-                echo "<div> <b>Result:</b> <br/> Correct <hr></div>";
-                $quizScore += 1;
-            } else {
-                echo "<div> <b>Question 6 Answer:</b> <br/> $question6 </div>";
-                echo "<div> <b>Result:</b> <br/> Incorrect <hr></div>";
-                $quizScore -= 1;
-            }
-        } else {
-            echo "<div> <b>Question 6 Answer:</b> <br/> Not answered <hr></div>";
-        };
-
-        //Validation on Q7 - must be specific string
-        if ( isset($_POST['question7']) ){
-            $question7 = $_POST['question7'];
-            if ($question7 === "George Washington"){
-                echo "<div> <b>Question 7 Answer:</b> <br/> $question7 </div>";
-                echo "<div> <b>Result:</b> <br/> Correct <hr></div>";
-                $quizScore += 1;
-            } else {
-                echo "<div> <b>Question 7 Answer:</b> <br/> $question7 </div>";
-                echo "<div> <b>Result:</b> <br/> Incorrect <hr></div>";
-                $quizScore -= 1;
-            }
-        } else {
-            echo "<div> <b>Question 7 Answer:</b> <br/> Not answered <hr></div>";
-        };
-
-        //Validation on Q8 - must be more than a certain number
-        if ( isset($_POST['question8']) ){
-            $question8 = $_POST['question8'];
-            if($question8 > 400){
-                echo "<div> <b>Question 8 Answer:</b> <br/> $question8 </div>";
-                echo "<div> <b>Result:</b> <br/> Correct <hr></div>";
-                $quizScore += 1;
-            } else {
-                echo "<div> <b>Question 8 Answer:</b> <br/> $question8 </div>";
-                echo "<div> <b>Result:</b> <br/> Incorrect <hr></div>";
-                $quizScore -= 1;
-            }
-        } else {
-            echo "<div> <b>Question 8 Answer:</b> <br/> Not answered <hr></div>";
-        };
-
-        //Validation on Q9 - just needs to check whether empty or not
-        if ( isset($_POST['question9']) ){
-            $question9 = $_POST['question9'];
-            echo "<div> <b>Question 9 Answer:</b> <br/> $question9 <hr></div>";
-        } else {
-            echo "<div> <b>Question 9 Answer:</b> <br/> Not answered <hr></div>";
-        };
-
-        //Validation on Q10 - needs to be exact number
-        //These will always be set and have default values
-        $question10Slider = $_POST['sliderOne'];
-        $question10NumberInput = $_POST['numberInputOne'];
-        $question10Result = $_POST['sliderOne'] + $_POST['numberInputOne'];
-        if ($question10Result === 150){
-            echo "<div> <b>Question 10 Answer:</b> <br/> $question10Result </div>";
-            echo "<div> <b>Result:</b> <br/> Correct <hr></div>";
-            $quizScore += 1;
-        } else {
-            echo "<div> <b>Question 10 Answer:</b> <br/> $question10Result </div>";
-            echo "<div> <b>Result:</b> <br/> Incorrect <hr></div>";
-            $quizScore -= 1;
-        };
-
-        //Validation on Q11 - needs to be exact number
-        //These will always be set and have default values
-        $question11Slider = $_POST['sliderTwo'];
-        $question11NumberInput = $_POST['numberInputTwo'];
-        $question11Result = $_POST['sliderTwo'] + $_POST['numberInputTwo'];
-        if ($question11Result === 600){
-            echo "<div> <b>Question 11 Answer:</b> <br/> $question11Result </div>";
-            echo "<div> <b>Result:</b> <br/> Correct <hr></div>";
-            $quizScore += 1;
-        } else {
-            echo "<div> <b>Question 11 Answer:</b> <br/> $question11Result </div>";
-            echo "<div> <b>Result:</b> <br/> Incorrect <hr></div>";
-            $quizScore -= 1;
-        };
+        echo "<pre>";
+        print_r($answers);
+        print_r($errors);
+        echo "</pre>";
     ?>
 
     <div>
